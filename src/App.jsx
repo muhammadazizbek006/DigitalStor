440a6482764874ee7a40324450f737c3d1c8ce80
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-05-06 15:27:37 +0300

    add dgitastore file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Home from './pages/Home'
+import Product from './pages/Product'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout/>}>
+        <Route index element={<Home/>}/>
+        <Route path='/Product' element={<Product/>}/>
+      </Route>
+    )
+  )
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const MainLayout = () => {
+  return (
+    <div>
+        <Header/>
+        <main>
+            <Outlet/>
+        </main>
+        <Footer/>
+    </div>
+  )
+}
+
+export default MainLayout
