89cf9b85eebab4beeb2a0dcb86c3f1f3da087ea2
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-05-08 10:00:59 +0300

    add trending components data map qilib chiqarildi

--- src/components/home/TrendingProducts.jsx
+++ src/components/home/TrendingProducts.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 // img
 import next from '../../img/home/next.svg'
+import { Trending } from '../../data'
 const TrendingProducts = () => {
   return (
     <>
@@ -15,6 +16,26 @@ const TrendingProducts = () => {
               <img src={next} alt="see more" />
             </div>
           </div>
+          {/* trending footwear map */}
+          <ul className='grid grid-cols-4 gap-x-6 gap-y-10'>
+            {
+              Trending.map((e)=>{
+                return(
+                  <li>
+                    <Link>
+                      <img className=' bg-white drop-shadow-lg pr-9 pb-7 mb-4 rounded' src={e.img} alt={e.brend} />
+                      <span className='text-sm font-bold text-input'>{e.brend}</span>
+                      <p className='text-2xl font-normal mb-1'>{e.title}</p>
+                      <div className='flex items-center'>
+                        <p className='mr-3 text-gray-600 text-2xl line-through'>{e.narx}</p>
+                        <p className='text-2xl font-bold '>{e.skidkanarx}</p>
+                      </div>
+                    </Link>
+                  </li>
+                )
+              })
+            }
+          </ul>
         </div>
       </section>
     </>
