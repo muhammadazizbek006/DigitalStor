fa46f7bde49e63fda89e5f6e36fa0762ac8d2381
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-05-08 10:45:27 +0300

    add cta

--- src/components/home/Cta.jsx
+++ src/components/home/Cta.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
+// img
+import cta from '../../img/home/cta.png'
 const Cta = () => {
   return (
     <>
-        <section>
-            <div className="containerb">
-                s
+        <section className='py-24'>
+            <div className="containerb flex justify-between">
+                {/* left */}
+                <img src={cta} alt="cta" />
+                {/* right */}
+                <div className='max-w-[589px]'>
+                    <p className='text-sm font-bold text-logo mb-4'>Oferta especial</p>
+                    <h2 className='text-5xl mb-5'>Air Jordan edição de colecionador</h2>
+                    <p className='text-base mb-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                </div>
             </div>
         </section>
     </>
