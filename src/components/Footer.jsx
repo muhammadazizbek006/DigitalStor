f5f1f8dcd68201a71cea483a32b0fadd0d1ad59f
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-05-08 10:57:11 +0300

    add footer left

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+// img
+import facebook from '../img/home/facebook.svg'
+import instagram from '../img/home/instagram.svg'
+import twitter from '../img/home/twitter.svg'
 
+import logofooter from '../img/home/logofooter.svg'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <section className='pt-20 pb-24 bg-[#1F1F1F] '>
+        <div className="containerb">
+          <ul className='text-white'>
+            {/* left */}
+            <li>
+              {/* logo */}
+                <div className='flex items-center mb-9'>
+                  <img className='mr-2' src={logofooter} alt="logofooter" />
+                  <h2 className='text-4xl font-semibold '>Digital Store</h2>
+                </div>
+                <p className='text-base max-w-72 mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                <div className='flex items-center space-x-9'>
+                  <a href="">
+                    <img src={facebook} alt="facebook" />
+                  </a>
+                  <a href="">
+                    <img src={instagram} alt="instagram" />
+                  </a>
+                  <a href="">
+                    <img src={twitter} alt="twitter" />
+                  </a>
+                </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
   )
 }
 
