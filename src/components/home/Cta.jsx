109b1c7b5e0d7bb72974748bde16733111551c11
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-05-12 14:45:40 +0300

    add cta responsive

--- src/components/home/Cta.jsx
+++ src/components/home/Cta.jsx
@@ -6,13 +6,13 @@ const Cta = () => {
   return (
     <>
         <section className='py-24'>
-            <div className="containerb flex justify-between">
+            <div className="containerb flex flex-col items-center md:items-start md:flex-row text-center md:text-start  md:justify-between">
                 {/* left */}
-                <img src={cta} alt="cta" />
+                <img className='mr-0 md:mr-5 w-96 mb-8 md:mb-0 md:w-[460px] lg:w-[600px] ' src={cta} alt="cta" />
                 {/* right */}
-                <div className='max-w-[589px]'>
-                    <p className='text-sm font-bold text-logo mb-4'>Oferta especial</p>
-                    <h2 className='text-5xl mb-5'>Air Jordan edição de colecionador</h2>
+                <div className='max-w-[459px]'>
+                    <p className='text-sm font-bold text-logo mb-1 md:mb-4'>Oferta especial</p>
+                    <h2 className=' text-4xl md:text-3xl lg:text-5xl mb-5'>Air Jordan edição de colecionador</h2>
                     <p className='text-base mb-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
                 </div>
             </div>
